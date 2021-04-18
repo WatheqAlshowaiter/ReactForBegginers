7ed56b65494df645307c0b7ff75eab4a5b5eabd1
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2021-04-18 03:33:52 +0300

    15 - Event Binding Callback

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "./Button";
 
+// class Test {
+// 	testing() {
+// 		console.log("testing....");
+// 		console.log(this);
+// 	}
+// }
+// let test = new Test();
+
+// setTimeout(() => {
+// 	test.testing();
+// }, 1000);
+
 function App() {
 	return (
 		<>
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 
 export default class Button extends Component {
-	constructor(props) {
-		super(props);
-		this.buttonClicked = this.buttonClicked.bind(this);
-	}
-	buttonClicked(e) {
+	buttonClicked = (e) => {
 		console.log("button clicked ");
 		console.log(e.type);
 		console.log(this);
-	}
+	};
 	render() {
 		return <button onClick={this.buttonClicked}>Click Me!</button>;
 	}
