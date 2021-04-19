9be7bf46eac23bef62e3995375b6f0335a84afc8
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2021-04-19 19:36:01 +0300

    20 - Children in depth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ListItems from "./smallerComponents/ListItems";
+import User from "./User";
+
+const userInfo = [<h2>Username</h2>, <h2>Age</h2>, <h2>Email</h2>];
+
+function listUserInfo(info) {
+	{
+		return (
+			<div>
+				<h1>ID: {info.id}</h1>
+				<h1>Name: {info.name}</h1>
+				<h1>Email: {info.email}</h1>
+			</div>
+		);
+	}
+}
 
 function App() {
 	return (
 		<>
-			<ListItems maxItems={10} />
+			<User>{listUserInfo}</User>
 		</>
 	);
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,28 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class User extends Component {
-	user = {
-		id: 1,
-		name: "Hasan",
-		email: "hasanemail@gmail.com",
-	};
-	renderUserInfo() {
-		return Object.keys(this.user).map((key) => {
-			const value = this.user[key];
-			return (
-				<div key={key}>
-					<strong>{key}: </strong>
-					<span>{value}</span>
-				</div>
-			);
-		});
-	}
-	render() {
-		return (
-			<div>
-				<h1>User info</h1>
-				{this.renderUserInfo()}
-			</div>
-		);
-	}
+const info = {
+	id: 1,
+	name: "hasan",
+	email: "email@email.com",
+};
+export default function User(props) {
+	return (
+		<div>
+			<h1>Users info</h1>
+			{props.children(info)}
+		</div>
+	);
 }
