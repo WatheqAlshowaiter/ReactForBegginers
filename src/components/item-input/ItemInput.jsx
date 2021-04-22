c7963fe186e9f7dd2819ede708dd2a460588565f
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2021-04-22 03:33:20 +0300

    7 - Checking List Items

--- src/components/item-input/ItemInput.jsx
+++ src/components/item-input/ItemInput.jsx
@@ -5,14 +5,16 @@ const ENTER_KEY = 13;
 const ESCAPE_KEY = 27;
 
 export default function ItemInput(props) {
-	let { onFinish, onClose, otherInputProps } = props;
-
+	let { onFinish, onClose, open, ...otherInputProps } = props;
+	if (open === false) return "";
 	const captureInputValue = (e) => {
 		let input = e.target,
 			value = input.value,
 			pressedButtonCode = e.keyCode || e.charCode;
 
 		if (pressedButtonCode === ENTER_KEY) {
+			if (value === "") return;
+			e.target.value = "";
 			onFinish(value);
 			value = "";
 		} else if (pressedButtonCode === ESCAPE_KEY) {
--- src/components/list-items/ListItems.jsx
+++ src/components/list-items/ListItems.jsx
@@ -2,10 +2,45 @@ import React, { Component } from "react";
 import ItemInput from "../item-input/ItemInput";
 import "./list-items.css";
 
+const itemIsCheckedByDefault = false;
+
+function DisplayItems(props) {
+	let { items, onCheck } = props;
+	console.log(items)
+
+	if (items.length === 0) return null;
+	const detectItemCheckStatus = (e, item, index) => {
+		item.checked = e.target.checked;
+
+		onCheck(item, index);
+	};
+	return (
+		<ul className="list-items">
+			{items.map((item, index) => {
+				let id = "item-checbox-" + index;
+				return (
+					<li className="list-item" key={index}>
+						<input
+							defaultChecked={itemIsCheckedByDefault}
+							onChange={(e) => detectItemCheckStatus(e, item, index)}
+							type="checkbox"
+							id={id}
+						/>
+						<label htmlFor={id}> {item.text}</label>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
+
 export default class ListItems extends Component {
 	state = {
+		name: "Todo",
 		items: [],
 		inputIsOpened: false,
+		totalCompleted : 0, 
+		
 	};
 
 	openInput = () => {
@@ -20,9 +55,27 @@ export default class ListItems extends Component {
 		});
 	};
 
+	updateItem = (item, index)=>{
+		let items = this.state.items,
+	
+		// update item 
+		items[index] = item; // a prolem here , do not know why
+	
+		let totalCompletedItems = items.reduce((totalChecked, item)=>{
+			return totalChecked + (item.checked ? 1: 0)
+		},0)
+
+		this.setState({
+			items
+		})
+	}
+
 	addNewItem = (value) => {
 		let items = this.state.items;
-		items.push(value);
+		items.push({
+			text: value,
+			checked: false,
+		});
 		this.setState({
 			items,
 		});
@@ -31,26 +84,22 @@ export default class ListItems extends Component {
 	render() {
 		return (
 			<div className="list-items-container">
-				<h1>List Items: {this.state.items.length}</h1>
+				<h1>
+					{this.state.name}: {this.state.totalCompleted} / {this.state.items.length}
+				</h1>
 				<button onClick={this.openInput} className="add-input-button">
 					Add Input
 				</button>{" "}
-				{this.state.inputIsOpened && (
-					<ItemInput
-						onFinish={this.addNewItem}
-						onClose={this.closeInput}
-						placeholder="writh list item then click Enter"
-					/>
-				)}
-				<ul className="list-items">
-					{this.state.items.map((item, index) => {
-						return (
-							<li className="list-item" key={index}>
-								{item}
-							</li>
-						);
-					})}
-				</ul>
+				<ItemInput
+					open={this.state.inputIsOpened}
+					onFinish={this.addNewItem}
+					onClose={this.closeInput}
+					placeholder="writh list item then click Enter"
+				/>
+				<DisplayItems
+					onCheck={this.updateItem}
+					items={this.state.items}
+				/>
 			</div>
 		);
 	}
